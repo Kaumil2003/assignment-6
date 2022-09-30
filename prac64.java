b04fee24ad21dc75c330359fdd4769d4d049d19c
Kaumil2003 <110337269+Kaumil2003@users.noreply.github.com>
2022-09-30 20:48:43 +0530

    Create prac64.java

---
+++ prac64.java
@@ -0,0 +1,34 @@
+package k1;
+
+/*
+  
+Write a program to create three threads ‘FIRST’, ‘SECOND’, ‘THIRD’. Set the priority of the ‘FIRST’ thread to 3, the ‘SECOND’ thread to 5(default) and the ‘THIRD’ thread to 7.
+
+ID : 21CE108
+NAME : Kaumil patni
+
+ */
+class Mythread extends Thread {
+    public void run() {
+        System.out.println("Thread " + Thread.currentThread().getName() + " is running");
+    }
+}
+
+public class prac64{
+    public static void main(String[] args) {
+        Mythread t1 = new Mythread();
+        Mythread t2 = new Mythread();
+        Mythread t3 = new Mythread();
+
+        t1.setName("First");
+        t2.setName("Second");
+        t3.setName("Third");
+        t1.setPriority(3);
+        t2.setPriority(5);
+        t3.setPriority(7);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
